458dab5c3de76b19036ad10bbe1048986afd0a53
Nitin <harehn.kaundun@mail.mcgill.ca>
2025-02-27 18:49:57 -0500

    simple reading csv file

--- reader.py
+++ reader.py
@@ -1,7 +1,20 @@
 import csv
 
-def read_one_column():
-    pass
+def read_all(filename):
+    fields = []
+    rows = []
+    with open(filename, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        fields = next(csvreader)
+        for row in csvreader:
+            rows.append(row)
+        return fields, rows
 
 if __name__ == "__main__":
     print("This life is amazing")
+    filename = 'colors.csv'
+    fields, rows = read_all(filename)
+    print(fields)
+    for row in rows:
+        # print(row)
+        print(row[0])
